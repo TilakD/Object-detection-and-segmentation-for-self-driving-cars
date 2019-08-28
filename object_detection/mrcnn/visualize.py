5abcae558cdc09add7a598678c107431cd412a4a
Tilak <tilakdevaraj@gmail.com>
2019-08-28 22:40:11 +0530

    Folder Setup and Mask RCNN Resnet101 with FPN

--- object_detection/mrcnn/visualize.py
+++ object_detection/mrcnn/visualize.py
@@ -163,8 +163,8 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-    if auto_show:
-        plt.show()
+    #if auto_show:
+    #    plt.show()
     return masked_image
 
 
